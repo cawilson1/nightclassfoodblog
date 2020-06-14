e19340b1ec77c9ec47ca356efab5b5ee90a7bdcc
cawilson1 <cawilson1@g.cofc.edu>
2020-06-14 10:19:15 -0400

    create foodblogpost db

--- dbseed.js
+++ dbseed.js
@@ -7,17 +7,34 @@ const pool = sql.createPool({
   password: "password1"
 });
 
-(async function createUserTable() {
+// (async function createUserTable() {
+//   try {
+//     const conn = await pool.getConnection();
+
+//     conn.query("CREATE DATABASE IF NOT EXISTS foodblog");
+//     conn.query("USE foodblog");
+
+//     const userDb = await conn.query(
+//       "CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) UNIQUE NOT NULL, profilepic VARCHAR(255), bio VARCHAR (3000), PRIMARY KEY(username) )"
+//     );
+//     console.log(userDb);
+
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+(async function createBlogPostTable() {
   try {
     const conn = await pool.getConnection();
 
-    conn.query("CREATE DATABASE IF NOT EXISTS foodblog");
     conn.query("USE foodblog");
 
-    const userDb = await conn.query(
-      "CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) UNIQUE NOT NULL, profilepic VARCHAR(255), bio VARCHAR (3000), PRIMARY KEY(username) )"
+    const foodblogpostDb = await conn.query(
+      "CREATE TABLE IF NOT EXISTS foodblogposts (id INT UNIQUE NOT NULL AUTO_INCREMENT, title VARCHAR(255), description VARCHAR(4095), username VARCHAR(255) NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id), FOREIGN KEY(username) REFERENCES user(username)  )"
     );
-    console.log(userDb);
+    console.log(foodblogpostDb);
 
     conn.release();
   } catch (error) {
